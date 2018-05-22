de5b24afd6a8200d763484077ccf5e4abff8ecc6
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-22 14:39:35 -0500

    finished seeder script on mysql: basic elements exercsise

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,12 +3,12 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
-  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(100),
-  name VARCHAR(100),
-  release_date INT(4),
-  sales DECIMAL(5,2),
-  genre VARCHAR(100),
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(128),
+  name VARCHAR(128),
+  release_date SMALLINT UNSIGNED,
+  sales FLOAT(6,2),
+  genre VARCHAR(128),
   PRIMARY KEY (id)
 
 );
