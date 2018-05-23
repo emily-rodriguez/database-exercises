b32cd4a856224c722cbc8dc1fbbaa05f4b99d3de
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-23 16:16:37 -0500

    created instance for each question instead of changing wording on same query clause

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,25 +2,20 @@ USE employees;
 
 SELECT * FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name DESC, first_name DESC;
+ORDER BY first_name;
 
 SELECT * FROM employees
-WHERE last_name LIKE 'E%';
-#
-SELECT * FROM employees
-WHERE hire_date LIKE '%199%';
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
 
 SELECT * FROM employees
-WHERE birth_date LIKE '%12-25%';
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
 
-SELECT * FROM employees
-WHERE last_name LIKE '%q%';
-
-SELECT * FROM employees
-WHERE (first_name = 'Irena'
-       OR first_name = 'Vidya'
-       OR first_name = 'Maya')
-      AND gender = 'M';
+SELECT *
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no;
 
 SELECT *
 FROM employees
@@ -28,19 +23,10 @@ WHERE last_name LIKE '%e'
   OR last_name LIKE 'e%'
 ORDER BY emp_no DESC;
 
-
-SELECT * FROM employees
-WHERE last_name LIKE 'e%e';
-
 SELECT *
 FROM employees
 WHERE birth_date LIKE '%12-25'
       AND hire_date LIKE '199%'
 ORDER BY birth_date ASC, hire_date DESC;
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%'
-      AND last_name NOT LIKE '%qu%';
 
 DESCRIBE employees;
