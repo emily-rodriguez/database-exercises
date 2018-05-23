46d6411ae808fa490494fea4f89f18f8f2bcc138
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-23 15:53:37 -0500

    order by exercise complete

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,8 @@
 USE employees;
+
 SELECT * FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name DESC, first_name DESC;
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%';
@@ -16,14 +18,15 @@ WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees
 WHERE (first_name = 'Irena'
-OR first_name = 'Vidya'
-OR first_name = 'Maya')
-AND gender = 'M';
+       OR first_name = 'Vidya'
+       OR first_name = 'Maya')
+      AND gender = 'M';
 
 SELECT *
 FROM employees
 WHERE last_name LIKE '%e'
-OR last_name LIKE 'e%';
+  OR last_name LIKE 'e%'
+ORDER BY emp_no DESC;
 
 
 SELECT * FROM employees
@@ -32,11 +35,12 @@ WHERE last_name LIKE 'e%e';
 SELECT *
 FROM employees
 WHERE birth_date LIKE '%12-25'
-AND hire_date LIKE '199%';
+      AND hire_date LIKE '199%'
+ORDER BY birth_date ASC, hire_date DESC;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+      AND last_name NOT LIKE '%qu%';
 
 DESCRIBE employees;
