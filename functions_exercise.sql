b098e4bd2c327fcfc0be1aa1375bba24e49f16bb
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-24 10:54:35 -0500

    mysql functions exercise, after class review

--- functions_exercise.sql
+++ functions_exercise.sql
@@ -1,10 +1,11 @@
-SELECT concat(first_name, ' ', last_name) AS FullName
+SELECT concat_ws(' ', first_name, last_name) AS FullName
 FROM employees
-WHERE last_name LIKE '%e'
-      AND last_name LIKE 'e%'
+WHERE first_name LIKE 'e%e'
+      AND last_name LIKE 'e%e'
 ORDER BY emp_no DESC;
 
-SELECT datediff(now(), hire_date)
+SELECT datediff(curdate(), hire_date) AS 'days',
+  (datediff(curdate(), hire_date) / 365.2422) AS 'years'
 FROM employees
 WHERE birth_date LIKE '%12-25'
       AND hire_date LIKE '199%';
