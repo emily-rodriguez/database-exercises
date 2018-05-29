864b8c8d8ab8c1f3d37f92b03bb365d980fa161c
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-29 11:06:02 -0500

    working on mysql bonuses

---
+++ world_database.sql
@@ -0,0 +1,32 @@
+USE world;
+
+# SELECT cl.language AS 'Language',
+#   cl.percentage AS 'Percentage'
+# FROM countrylanguage cl
+# JOIN country c ON cl.CountryCode = c.code
+# JOIN city ci ON ci.CountryCode = c.code
+# WHERE ci.name = 'Santa Monica'
+# ORDER BY Percentage;
+#
+# SELECT c.region AS 'Region',
+#   count(c.name) AS 'num_countries'
+# FROM country c
+# GROUP BY Region;
+
+# SELECT c.region AS 'Region',
+#   sum(c.population) AS 'population'
+# FROM country c
+# GROUP BY Region;
+
+# SELECT c.continent AS 'Continent',
+#   sum(c.population) AS 'population'
+# FROM country c
+# GROUP by Continent;
+#
+# SELECT avg(c.lifeexpectancy) FROM country c
+
+SELECT c.continent AS 'Continent',
+  avg(c.lifeexpectancy) AS 'life_expectancy'
+FROM country c
+GROUP BY Continent;
+
